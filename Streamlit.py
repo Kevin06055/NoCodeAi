e66449bbcbe2205a36fe75b410718a6bcfeb23a4
Gamixpro2424 <112698224+Gamixpro2424@users.noreply.github.com>
2024-02-14 22:05:30 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import string
 import random 
-a = random.sample(string.digits,5)
-print(a)
-st.write("My First APP- Generated NUmber",int(a))
+
+st.write("My First APP- Generated NUmber",string.digits)
